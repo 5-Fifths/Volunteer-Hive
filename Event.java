993d259a01b7d821115105532e269c6622d5e891
5-Fifths <47290088+5-Fifths@users.noreply.github.com>
2024-02-22 17:46:39 -0800

    Base classes

---
+++ Event.java
@@ -0,0 +1,16 @@
+
+// Event class stores information about the event
+
+public class Event {
+    // Event information
+    // Coordinator in charge
+    // Date, Time
+    // Location
+    // Volunteer list
+        // Size limit
+    // Open/closed/semi-closed status
+        // Open: Anyone can join
+        // Semi-closed: space-limited or by appilication only
+        // Closed: Invitation only
+    // Other details
+}
---
+++ Volunteer.java
@@ -0,0 +1,6 @@
+public class Volunteer {
+    // Volunteers can sign up for events
+    public void signUp(Event event) {
+        
+    }
+}
