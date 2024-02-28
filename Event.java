8a9f5a2328cdfa8aa788263892b20a6c76aba8e7
KyleV14 <94767629+KyleV14@users.noreply.github.com>
2024-02-27 20:01:55 -0800

    Update Event.java

--- Event.java
+++ Event.java
@@ -25,8 +25,73 @@ public class Event {
 public Event (String eventName, String coordinator, Int date, Int time, String location, String[] volunteerList, Int sizeLimit, String status) {
 
     this.eventName = eventName; 
+    this.coordinator = coordinator;
+    this.date = date;
+    this.time = time;
+    this.location = location;
+    this.volunteerList = new String[sizeLimit];
+    this.sizeLimit = sizeLimit;
+    this.status = status;
+    }
     
+public String getEvent{
+    return this.eventName;
+} 
 
+public String getCoord{
+    return this.coordinator;
+}
 
+public int getDate{
+    return this.date;
+}
+    
+public int getTime{
+    return this.time;
+}
+
+public String getLoc{
+    return this.location;
+}
+
+public String getVolList{
+    return this.volunteerList;
+}
+
+public int getSizeL{
+    return this.sizeLimit;
+}
+    
+public String getStatus{
+    return this.status;
+}
+
+public void addVolunteer(String volunteer) {
+    for (int i = 0; i < volunteerList.length; i++){
+        if (volunteerList[i] == null){
+            volunteerList[i] = volunteer;
+            return;
+        }
+    }
+}
+
+public void volunteerListOut(){
+    for (int j = 0; j < volunteerList.length; j++){
+        String volunteers = volunteerList[j];
+        if (volunteers != null){
+            System.out.println(volunteers);
+        }
+    }
+}
+
+public boolean isFull() {
+    for (int k = 0; k < volunteerList.length; k++) {
+        if (volunteerList[k] == null){
+            status = "OPEN";
+            return false;
+    }
+        status = "CLOSED";
+        return true;
+}
     
 }
