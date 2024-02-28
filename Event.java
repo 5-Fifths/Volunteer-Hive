58beab127902331c0b67e6c3153d7a4348d0544e
KyleV14 <94767629+KyleV14@users.noreply.github.com>
2024-02-27 19:29:33 -0800

    Update Event.java

--- Event.java
+++ Event.java
@@ -3,14 +3,30 @@
 
 public class Event {
     // Event information
+    private String eventName;
     // Coordinator in charge
+    private String coordinator;
     // Date, Time
+    private Int date;
+    private Int time;
     // Location
+    private String location
     // Volunteer list
+    private String[] volunteerList
         // Size limit
+    private Int sizeLimit
     // Open/closed/semi-closed status
+    private String status
         // Open: Anyone can join
         // Semi-closed: space-limited or by appilication only
         // Closed: Invitation only
     // Other details
+
+public Event (String eventName, String coordinator, Int date, Int time, String location, String[] volunteerList, Int sizeLimit, String status) {
+
+    this.eventName = eventName; 
+    
+
+
+    
 }
