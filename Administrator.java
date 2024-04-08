81711a26f73d8c00acafb946bb127a43b6f1fc3b
danymany1 <159497985+danymany1@users.noreply.github.com>
2024-04-08 15:16:27 -0700

    Update and rename Staff.java to Administrator.java

--- Administrator.java
+++ Administrator.java
@@ -1,9 +1,9 @@
-interface Staff {
-    // Staff can invite and remove users from events
+interface Administrator {
+    // Administrator can invite and remove users from events
     public void userInvitation(User user, Event event);
     public void userRemoval(User user, Event event);
 
-    // Staff can create, remove, and edit events
+    // Administrator can create, remove, and edit events
     public Event createEvent(); // Add the minimum required information to create a basic Event
     public void removeEvent(Event event);
     public void editEvent(); // Parameters currently unclear
