4fda20896a9cd35313c6766dc8bdbfdd1bd3946c
danielespinozadiaz <159494002+danielespinozadiaz@users.noreply.github.com>
2024-04-25 22:10:25 -0700

    Added password hashing

--- database.js
+++ database.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require("body-parser");
 const encoder = bodyParser.urlencoded();
+const bcrypt = require('bcrypt');
+const saltRounds = 5;
 
 const app = express();
 
@@ -29,28 +31,73 @@ app.get("/login", (req, res) => {
 app.post("/register", encoder, function(req, res){
     var username = req.body.username;
     var password = req.body.password;
-    con.query("INSERT INTO login (username, password) values (?, ?)", [username, password], function(error, results, fields){
-        if(results.affectedRows > 0){
-            res.redirect("/login");
-        } else {
+    console.log("\nUsername: %s\nPassword: %s", username, password);
+    bcrypt.hash(password, saltRounds, (error, hash) =>{
+        if(error) {
+            console.error(error);
             res.redirect("/");
+            return;
         }
-        res.end();
+        console.log("\nUsername: %s\nHashed Password: %s", username, hash);
+        con.query("INSERT INTO login (username, password) values (?, ?)", [username, hash], function(error, results, fields){
+            if(error) {
+                console.error(error);
+                res.redirect("/");
+                return;
+            }
+            if(results.affectedRows > 0){
+                res.redirect("/login");
+            } else {
+                res.redirect("/");
+            }
+            res.end();
+        });
     });
 });
 
+
 app.post("/login", encoder, function(req, res){
+    // Retrieve username and password from the request body
     var username = req.body.username;
     var password = req.body.password;
-    con.query("SELECT * FROM login WHERE username = ? AND password = ?", [username, password], function(error, results, fields){
+    
+    // Query the database to find a user with the provided username
+    con.query("SELECT * FROM login WHERE username = ?", [username], function(error, results, fields){
+        // Message to indicate that password verification is in progress
+        console.log("\nVerifying password...");
+        
+        // If a user with the provided username is found
         if(results.length > 0){
-            res.redirect("/hope");
+            // Extract the hashed password for the user
+            var storedHash = results[0].password;
+            
+            // Compare the hashed password with the user's input password
+            bcrypt.compare(password, storedHash, function(err, result) {
+                // If the passwords match, redirect to the "/hope" page
+                if(result) {
+                    res.redirect("/hope");
+                    // Message to indicate that passwords match
+                    console.log("\nPasswords match!");
+                } else {
+                    // If passwords do not match, redirect to the login page "/"
+                    res.redirect("/");
+                }
+                // End the response
+                res.end(); 
+            });
+
+        /* ---- Didn't make it past results.length check ---- */
         } else {
+            // If no user with the provided username is found, redirect to the login page "/"
             res.redirect("/");
+            // End the response
+            res.end(); 
         }
-        res.end();
     });
 });
+
+
+
 
 app.get("/hope", function(req, res){
     res.sendFile(__dirname + "/hope.html")
