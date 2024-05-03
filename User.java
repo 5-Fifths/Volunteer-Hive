97a7dfbeced45d9a3d462a921a8ad2537b2d38d2
brian <btruong648@gmail.com>
2024-05-02 21:06:07 -0700

    idk

--- User.java
+++
@@ -1,8 +0,0 @@
-interface User {
-    // Users can change their profile information
-    public void editBio();
-    public void editSkills(String[] skills);
-    public void editPreferredName(String newName);
-
-    // Add more as necessary
-}
